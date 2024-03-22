618c7b1868b8d31e384f562fcbee1703d2187980
Tiago Cepeda <133398870+tcepeda@users.noreply.github.com>
2024-03-22 09:36:43 +0000

    Update bacia_hidrografica.py

--- src/bacia_hidrografica.py
+++ src/bacia_hidrografica.py
@@ -167,7 +167,7 @@ px.set_mapbox_access_token(mapbox_access_token)
 # MAIN CONTAINER----------------------------------------------------------------
 formatted_text = f"""<h2 style="font-weight: bold;">
 <span style="font-size: 60px; color: #1D70B7;">Explora o Mapa</span> <br>
-<span style="color: #35A8E0;">Parâmetro: <span style="color: #B2B2B2;">{variable_name} {si_units}</span></span></h2>"""
+<span style="color: #B2B2B2;">Parâmetro: <span style="color: #35A8E0;">{variable_name} {si_units}</span></span></h2>"""
 
 st.markdown(formatted_text, unsafe_allow_html=True)
 
